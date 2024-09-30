0b9b4add4790724285e84edae0ed2ed6f16219b3
Charles Boye <157924508+CharlieBoye8@users.noreply.github.com>
2024-09-30 11:32:09 -0400

    Completed 04_03

--- Ch04/04_03/start/react-app/src/App.js
+++ Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 
-function App() {
+const [firstCity, second] =["Tokyo", "Tahoe City", "Bend"]
+console.log(second)
+console.log(firstCity)
+
+function App({library}) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
